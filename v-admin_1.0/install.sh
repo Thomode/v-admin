c8026808f7875591faf7de935de026e2115e0630
Thomode <figueroalopeztomas@gmail.com>
2021-06-04 13:56:24 -0300

    Update install.sh

--- v-admin_1.0/install.sh
+++ v-admin_1.0/install.sh
@@ -1,2 +1,5 @@
-cd
-wget https://raw.githubusercontent.com/Thomode/v-admin/main/v-admin_1.0/v-admin.py
+#!/bin/bash
+wget -O /home/v-admin.py https://raw.githubusercontent.com/Thomode/v-admin/main/v-admin_1.0/v-admin.py
+wget -O /bin/menuV1 https://raw.githubusercontent.com/AAAAAEXQOSyIpN2JZ0ehUQ/SSHPLUS-MANAGER-FREE/master/Install/menuV1 > /dev/null 2>&1; chmod +x /bin/menuV1
+
+wget -O /bin/v-admin https://raw.githubusercontent.com/Thomode/v-admin/main/v-admin_1.0/v-admin; chmod +x /bin/menuV1

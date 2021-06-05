b5c3d80a79dab0e42dcf2cc6e9eeb0f814decacd
Thomode <figueroalopeztomas@gmail.com>
2021-06-05 16:55:26 -0300

    Update install.sh

--- v-admin_1.1/install.sh
+++ v-admin_1.1/install.sh
@@ -2,7 +2,7 @@
 
 rm /home/v-admin.py
 rm /bin/v-admin
-wget -O /home/v-admin.py https://raw.githubusercontent.com/Thomode/v-admin/main/v-admin_1.0/v-admin.py
-wget -O /bin/v-admin https://raw.githubusercontent.com/Thomode/v-admin/main/v-admin_1.0/v-admin; chmod +x /bin/v-admin
+wget -O /home/v-admin.py https://raw.githubusercontent.com/Thomode/v-admin/main/v-admin_1.1/v-admin.py
+wget -O /bin/v-admin https://raw.githubusercontent.com/Thomode/v-admin/main/v-admin_1.1/v-admin; chmod +x /bin/v-admin
 
 rm install.sh
